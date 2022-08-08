b9b9c761e77f3771ae80f2c316d09dcdaa7ed594
vladislav.borejsho <bjiadisjiav@gmail.com>
2022-08-08 09:38:25 +0300

    task 50% done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
+  const BRACKETS_PAIR = {
+    [')'] : '(',
+    ['}'] : '{',
+    [']'] : '[',
+    ['|'] : '|',
+    ['2'] : '1',
+    ['4'] : '3',
+    ['6'] : '5',
+    ['7'] : '7',
+    ['8'] : '8',
+  }
   // your solution
+  let stack = [];
+  for (let i=0; i<str.length; i++) {
+    let currentSymbol = str[i]; 
+    if (bracketsConfig.includes(currentSymbol) && stack.length === 0) {
+      stack.push(currentSymbol); 
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      let topElement = stack[stack.length - 1]; 
+      if (BRACKETS_PAIR[currentSymbol] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+return stack.length === 0;
 }
+
+//check('()', config1), true);
