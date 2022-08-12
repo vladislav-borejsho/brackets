b942dd4063a3b5cd6fe0029ece6c19487884d2dd
vladislav.borejsho <bjiadisjiav@gmail.com>
2022-08-13 00:56:39 +0300

    20/20 done

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  let BRACKETS_PAIR = [];
-  for (let i = 0; i < bracketsConfig.length; i++) {
-        BRACKETS_PAIR.push(bracketsConfig[i][1])
-  } 
+  let BRACKETS_PAIR = {
+    ')' : '(',
+    '}' : '{',
+    ']' : '[',
+    '|' : '|',
+    '2' : '1',
+    '4' : '3',
+    '6' : '5',
+    '7' : '7',
+    '8' : '8',
+  }
     let stack = [];
     for (let i=0; i<str.length; i++) {
       let currentSymbol = str[i];
-      if (stack.length === 0 ) {
-      stack.push(currentSymbol); 
-      console.log(stack); 
-      } else {
-        let topElement = stack[stack.length - 1];
-              if (BRACKETS_PAIR.includes(currentSymbol) || currentSymbol === topElement || stack.length !== 0) {
+      topEl = stack[stack.length -1];
+        if (BRACKETS_PAIR[currentSymbol] === topEl && stack.length !== 0) {
           stack.pop();
-        } else {
-          return false;
-        }
-      }
+      } else if (BRACKETS_PAIR[currentSymbol] !== topEl || stack.length === 0) { 
+        stack.push(currentSymbol); 
+      } 
     }
-  return stack.length === 0;
+      return stack.length === 0;
 }
-
-//check('()', config1), true);
